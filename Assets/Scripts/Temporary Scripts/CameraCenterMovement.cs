0fe00e121c875280ca2aef8779b7432599842716
darklaw53 <marceloschulze@gmail.com>
2023-07-30 00:24:41 -0300

    added camera bounds

--- Assets/Scripts/Temporary Scripts/CameraCenterMovement.cs
+++ Assets/Scripts/Temporary Scripts/CameraCenterMovement.cs
@@ -1,3 +1,14 @@
+/*
+ * CameraControler
+ * 
+ * Initial setup done by Hellhound. Confining camera to bounds, adding border push and double clicking to follow units
+ * 
+ * Authors: Hellhound, Archetype
+ * 
+ * Changes: 
+ *  [07/29/2023] replaced "CameraMovement" script with cinemachine
+ * 
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +18,28 @@ public class CameraCenterMovement : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb2d;
     public Vector2 moveInput;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject cam;
 
-    // Update is called once per frame
+    //controlls position of camera target (as seen in the virtual camera follow)
     void Update()
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize();
+    }
 
+    private void FixedUpdate()
+    {
         rb2d.velocity = moveInput * moveSpeed;
+
+        if (transform.position.x > cam.transform.position.x + 5 || transform.position.x < cam.transform.position.x - 5)
+        {
+            transform.position = new Vector3(cam.transform.position.x, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.y > cam.transform.position.y + 5 || transform.position.y < cam.transform.position.y - 5)
+        {
+            transform.position = new Vector3(transform.position.x, cam.transform.position.y, transform.position.z);
+        }
     }
 }
