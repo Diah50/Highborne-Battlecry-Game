37e46cae59cf4448e7dd2eda84824560db35bee4
InfuriatedBrute <bobitron74@gmail.com>
2024-03-06 23:43:47 -0500

    Refactor

--- godot/Scripts/CameraManagement.cs
+++ godot/Scripts/CameraManagement.cs
@@ -4,49 +4,43 @@ using System.Numerics;
 
 public partial class CameraManagement : Node2D
 {
-	public double ZOOM_SPEED = 5; // in current zoom per second
-	public int MIN_ZOOM = 1; // higher zoom = more zoomed in
-	public int MAX_ZOOM = 100;
-	public double PAN_SPEED = 300; // in units per second
-	public double DOUBLETAP_MSECS = 500;
+    public double ZOOM_SPEED = 5; // in current zoom per second
+    public int MIN_ZOOM = 1; // higher zoom = more zoomed in
+    public int MAX_ZOOM = 100;
+    public double PAN_SPEED = 300; // in units per second
+    public double DOUBLETAP_MSECS = 500;
 
-	private int pfps;
-	private double pdelta;
-	private Camera2D camera;
+    private int pfps;
+    private double pdelta;
+    private Camera2D camera;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		pfps = (int) ProjectSettings.GetSetting("physics/common/physics_fps");
-		pdelta = 1.0/pfps;
-		camera = GetNode<Camera2D>("Camera2D");
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        pfps = (int)ProjectSettings.GetSetting("physics/common/physics_fps");
+        pdelta = 1.0 / pfps;
+        camera = GetNode<Camera2D>("Camera2D");
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		CameraInput(camera, delta);
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        CameraInput(camera, delta);
+    }
 
-	public void CameraInput(Camera2D c, double delta){
-		var pan = (float) (PAN_SPEED * c.Zoom.X * delta);
-		var zoom = (float) (ZOOM_SPEED * delta);
-		if (Input.IsActionPressed("pan_left"))
-			c.MoveLocalX(-pan);
-		if (Input.IsActionPressed("pan_right"))
-			c.MoveLocalX(pan);
-		if (Input.IsActionPressed("pan_up"))
-			c.MoveLocalY(-pan);
-		if (Input.IsActionPressed("pan_down"))
-			c.MoveLocalY(pan);
-		if (Input.IsActionJustReleased("zoom_in"))
-			Zoom(zoom, c);
-		if (Input.IsActionJustReleased("zoom_out"))
-			Zoom(-zoom, c);
-	}
+    public void CameraInput(Camera2D c, double delta)
+    {
+        float panIncrement = (float)(PAN_SPEED * c.Zoom.X * delta);
+		float zoomIncrement = (float)(ZOOM_SPEED * delta);
 
-	private void Zoom(float i, Camera2D c){
-		float scale = 1 + i;
-		camera.Zoom = new Godot.Vector2(camera.Zoom.X * scale, camera.Zoom.Y * scale);
-	}
+        var pan = new Godot.Vector2(
+            ((Input.IsActionPressed("pan_right") ? 1 : 0) - (Input.IsActionPressed("pan_left") ? 1 : 0)) * panIncrement,
+            ((Input.IsActionPressed("pan_down") ? 1 : 0) - (Input.IsActionPressed("pan_up") ? 1 : 0)) * panIncrement);
+        c.Position += pan;
+
+        float zoom = 1f + 
+			((Input.IsActionJustReleased("zoom_in") ? 1 : 0) - (Input.IsActionJustReleased("zoom_out") ? 1 : 0)) 
+			* zoomIncrement;
+		c.Zoom *= zoom;
+    }
 }
