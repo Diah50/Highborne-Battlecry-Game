aca0c7e41760d9dc56ef80ee03794c59f7293e19
InfuriatedBrute <bobitron74@gmail.com>
2024-03-09 19:58:13 -0500

    Changes for reducing camera stutter and input ghosting

--- godot/Scripts/CameraManagement.cs
+++ godot/Scripts/CameraManagement.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 public partial class CameraManagement : Node2D
@@ -13,12 +15,27 @@ public partial class CameraManagement : Node2D
     private int pfps;
     private double pdelta;
     private Camera2D camera;
+    private HashSet<string> holdableActions = new HashSet<string>() { "pan_left", "pan_right", "pan_up", "pan_down" };
+
+    private HashSet<string> heldActions = new HashSet<string>();
+
+
+    public override void _Input(InputEvent @event)
+    {
+        // reduces ghosting on held actions compared to Input.IsActionPressed
+        // not necessary for presses, just use Input.IsActionJustPressed
+        var newlyHeldActions = holdableActions.Except(heldActions).Where(action => @event.IsActionPressed(action));
+        heldActions = heldActions.Concat(
+                newlyHeldActions
+            )
+            .Where(action => !@event.IsActionReleased(action))
+            .ToHashSet();
+    }
+
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        pfps = (int)ProjectSettings.GetSetting("physics/common/physics_fps");
-        pdelta = 1.0 / pfps;
         camera = GetNode<Camera2D>("Camera2D");
     }
 
@@ -31,17 +48,21 @@ public partial class CameraManagement : Node2D
     public void CameraInput(Camera2D c, double delta)
     {
         float panIncrement = (float)(PAN_SPEED * delta / c.Zoom.X);
-		float zoomIncrement = (float)(ZOOM_SPEED * delta);
+        float zoomIncrement = (float)(ZOOM_SPEED * delta);
 
         var pan = new Godot.Vector2(
-            ((Input.IsActionPressed("pan_right") ? 1 : 0) - (Input.IsActionPressed("pan_left") ? 1 : 0)) * panIncrement,
-            ((Input.IsActionPressed("pan_down") ? 1 : 0) - (Input.IsActionPressed("pan_up") ? 1 : 0)) * panIncrement);
-        c.Position += pan;
+            ((heldActions.Contains("pan_right") ? 1 : 0) - (heldActions.Contains("pan_left") ? 1 : 0)) * panIncrement,
+            ((heldActions.Contains("pan_down") ? 1 : 0) - (heldActions.Contains("pan_up") ? 1 : 0)) * panIncrement);
+        if (pan != Godot.Vector2.Zero)
+            c.Position += pan;
 
-        float zoomChange = 1f + 
-			((Input.IsActionJustReleased("zoom_in") ? 1 : 0) - (Input.IsActionJustReleased("zoom_out") ? 1 : 0)) 
-			* zoomIncrement;
-		float zoom = Mathf.Clamp(c.Zoom.X * zoomChange, MIN_ZOOM, MAX_ZOOM);
-		c.Zoom = new Godot.Vector2(zoom, zoom);
+        float zoomMult = 1f +
+            ((Input.IsActionJustPressed("zoom_in") ? 1 : 0) - (Input.IsActionJustPressed("zoom_out") ? 1 : 0))
+            * zoomIncrement;
+        if (zoomMult != 1f)
+        {
+            float zoom = Mathf.Clamp(c.Zoom.X * zoomMult, MIN_ZOOM, MAX_ZOOM);
+            c.Zoom = new Godot.Vector2(zoom, zoom);
+        }
     }
 }
